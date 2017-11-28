c64a797d3080bc26b5a818c6ca0444457e5a6fba
theharveyz <geekwebme@gmail.com>
2017-11-28 17:20:29 +0800

    add ...

--- src/syntax/modules/test/a.lua
+++ src/syntax/modules/test/a.lua
@@ -2,11 +2,12 @@ local _M = {}
 
 function _M:test ( ... )
     -- 获取展开式可变参数
-    local args = {...}
+    local _args = ... --  第一个参数？
+    local args = {...} -- 转成table
     for i=1, #args do
         print("\n" .. args[i])
     end
-
+    print(_args)
     return args
 end
 
